74a2a6ac3d65469de731e8743c1b95b18f521432
Ryan Cheley <rcheley@gmail.com>
2022-02-03 21:11:59 -0800

    added test

---
+++ acronyms/tests/test_models.py
@@ -0,0 +1,13 @@
+import pytest
+
+from acronyms.models import Acronym
+
+
+@pytest.mark.django_db
+def test_string_representation():
+    acronym = 'cms'
+    definition = 'Centers for Medicare and Medicaid Services'
+    value = Acronym.objects.get_or_create(acronym=acronym, definition=definition)
+    expected = acronym
+    actual = str(value[0])
+    assert actual == expected
