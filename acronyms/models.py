1072536d9b36cd735ceb7352c62c013fff8e89c6
Ryan Cheley <rcheley@gmail.com>
2022-02-04 06:48:25 -0800

    added constraint to model to prevent duplicates

--- acronyms/models.py
+++ acronyms/models.py
@@ -6,5 +6,14 @@ class Acronym(models.Model):
     acronym = models.CharField(max_length=8)
     definition = models.TextField()
 
+    def save(self, *args, **kwargs):
+        self.acronym = self.acronym.lower()
+        super(Acronym, self).save(*args, **kwargs)
+
+
+    class Meta:
+        unique_together = ('acronym', 'definition')
+
+
     def __str__(self) -> str:
         return self.acronym
