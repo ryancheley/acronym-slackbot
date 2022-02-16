201922524b78b1b4cbfd36fba4456fae42985c96
Ryan Cheley <rcheley@gmail.com>
2022-02-16 06:49:36 -0800

    updated test for new field

--- acronyms/tests/test_models.py
+++ acronyms/tests/test_models.py
@@ -7,7 +7,9 @@ from acronyms.models import Acronym
 def test_string_representation():
     acronym = "cms"
     definition = "Centers for Medicare and Medicaid Services"
-    value = Acronym.objects.get_or_create(acronym=acronym, definition=definition)
+    create_by = "test_user"
+    approved = True
+    value = Acronym.objects.get_or_create(acronym=acronym, definition=definition, create_by=create_by, approved=approved)
     expected = acronym
     actual = str(value[0])
     assert actual == expected
