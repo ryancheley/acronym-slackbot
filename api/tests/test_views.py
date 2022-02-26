f82cc782381631fedaf4a32019099f537b130190
Ryan Cheley <rcheley@gmail.com>
2022-02-26 08:38:52 -0800

    added tests for #47

---
+++ api/tests/test_views.py
@@ -0,0 +1,62 @@
+import pytest
+from django.conf import settings
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from acronyms.models import Acronym
+
+SLACK_VERIFICATION_TOKEN = getattr(settings, "SLACK_VERIFICATION_TOKEN", None)
+
+
+@pytest.mark.django_db
+def test_get_acronym_success():
+    Acronym.objects.create(acronym="aca", definition="Affordable Care Act", create_by="lauren", approved=True)
+    url = reverse("api:acronym-detail", kwargs={"acronym": "aca"})
+    client = APIClient()
+    request = client.get(url)
+    assert request.status_code == 200
+
+
+@pytest.mark.django_db
+def test_get_acronym_failure():
+    Acronym.objects.create(acronym="aca", definition="Affordable Care Act", create_by="lauren", approved=True)
+    url = reverse("api:acronym-detail", kwargs={"acronym": "ftf"})
+    client = APIClient()
+    request = client.get(url)
+    assert request.status_code == 404
+
+
+def test_get_acronym_verification_failure():
+    client = APIClient()
+    url = reverse("api:events")
+    request = client.post(url)
+    assert request.status_code == 403
+
+
+def test_get_acronym_verification_success():
+    client = APIClient()
+    url = reverse("api:events")
+    data = {"token": SLACK_VERIFICATION_TOKEN, "type": "url_verification"}
+    request = client.post(url, data)
+    assert request.status_code == 200
+
+
+@pytest.mark.django_db
+def test_slash_function_count_with_zero_results():
+    client = APIClient()
+    client.login(username="lauren", password="secret")
+    request = client.post("/api/slack/count/")
+    actual = request.data
+    expected = "There are 0. Here is a random one None"
+    assert actual == expected
+
+
+@pytest.mark.django_db
+def test_slash_function_count_with_non_zero_results():
+    Acronym.objects.create(acronym="aca", definition="Affordable Care Act", create_by="lauren", approved=True)
+    client = APIClient()
+    client.login(username="lauren", password="secret")
+    request = client.post("/api/slack/count/")
+    actual = request.data
+    expected = "There are 1. Here is a random one aca"
+    assert actual == expected
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 import ssl
 
-import requests
 import slack
 from django.conf import settings
 from django.shortcuts import get_object_or_404
@@ -11,7 +10,7 @@ from rest_framework.views import APIView
 from acronyms.models import Acronym
 
 from .serializers import AcronymSerializer
-from .utils import acronym_checker, string_split
+from .utils import format_checker, process_user_message, string_split
 
 ssl_context = ssl.create_default_context()
 ssl_context.check_hostname = False
@@ -45,52 +44,19 @@ class CountAcronyms(APIView):
 
 
 class AddAcronym(APIView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):  # pragma: no cover
         request_data = string_split(request.data["text"])
-        acronym = request_data[0]
         channel = request.data["channel_id"]
-        try:
-            acronym_checker(acronym=acronym)
-        except AttributeError:
-            message = f"Acronyms need to be 8 characters or fewer. The acronym '{acronym}' is {len(acronym)}. Please try again."
-            client.chat_postMessage(blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": message}}], channel=channel)
-            return Response(status=status.HTTP_201_CREATED)
-        except TypeError:
-            message = "The format of the acronym add needs to be 'acronym: definition'."
-            message += "'\nFor example:\n\t*example: this is an example.*\nPlease try again."
-            client.chat_postMessage(blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": message}}], channel=channel)
-            return Response(status=status.HTTP_201_CREATED)
-        definition = request_data[1]
-        check_for_acronym = Acronym.objects.filter(acronym=acronym).exists()
-        if not check_for_acronym and acronym and definition:
-            user = request.data["user_name"]
-            message = f"The acronym *{acronym.upper()}* with definition '{definition}' has been added!"
-            client.chat_postMessage(blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": message}}], channel=channel)
-            record = Acronym.objects.create(
-                acronym=acronym,
-                definition=definition,
-                create_by=user,
-                approved=False,
-            )
-            record.save()
-            return Response(status=status.HTTP_201_CREATED)
-        elif not acronym or not definition:
-            message = "You have entered the data incorrectly!"
-            client.chat_postMessage(blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": message}}], channel=channel)
-            return Response(status=status.HTTP_200_OK)
-        else:
-            message = f"The acronym {acronym} with definition {definition} already exists!"
-            client.chat_postMessage(blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": message}}], channel=channel)
-            return Response(status=status.HTTP_200_OK)
-
-            # return Response(status=status.HTTP_409_CONFLICT)
+        message, response_status = format_checker(request_data)
+        blocks = [{"type": "section", "text": {"type": "mrkdwn", "text": message}}]
+        client.chat_postMessage(blocks=blocks, channel=channel)
+        return Response(status=response_status)
 
 
 class Events(APIView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):  # pragma: no cover
 
         slack_message = request.data
-
         if slack_message.get("token") != SLACK_VERIFICATION_TOKEN:
             return Response(status=status.HTTP_403_FORBIDDEN)
 
@@ -100,6 +66,7 @@ class Events(APIView):
         # greet bot
         if "event" in slack_message:
             event_message = slack_message.get("event")
+            print(event_message)
 
             # ignore bot's own message
             if event_message.get("subtype"):
@@ -108,18 +75,10 @@ class Events(APIView):
             # process user's message
             user = event_message.get("user")
             text = event_message.get("text")
-            channel = event_message.get("channel")
-            url = f"https://slackbot.ryancheley.com/api/{text}/"
-            response = requests.get(url).json()
-            definition = response.get("definition")
-            if definition:
-                message = f"The acronym '{text.upper()}' means: {definition}"
-            else:
-                confluence = CONFLUENCE_LINK + f'/dosearchsite.action?cql=siteSearch+~+"{text}"'
-                confluence_link = f"<{confluence}|Confluence>"
-                message = f"I'm sorry <@{user}> I don't know what *{text.upper()}* is :shrug:. Try checking {confluence_link}."
+            message = process_user_message(text, user)
 
             if user != "U031T0UHLH1":
+                channel = event_message.get("channel")
                 client.chat_postMessage(
                     blocks=[{"type": "section", "text": {"type": "mrkdwn", "text": message}}], channel=channel
                 )
