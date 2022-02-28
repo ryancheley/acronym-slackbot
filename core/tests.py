16f6bff8cdbdf2a940802d08ec643c80848ee18c
Ryan Cheley <rcheley@gmail.com>
2022-02-27 16:09:34 -0800

    library updates and tests

---
+++ core/tests.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django_permissions_policy.__init__ import FEATURE_NAMES
+
+PERMISSIONS_POLICY = list(getattr(settings, "PERMISSIONS_POLICY", None))
+
+
+def test_check_current_permission_policy_items():
+    """
+    Check to make sure that the keys in the current PERMISSIONS_POLICY exist
+    in the version of django-permissions-policy. This will help to keep you
+    from upgrading to a version that will generate 500 errors on your website
+    """
+    for i in PERMISSIONS_POLICY:
+        assert i in FEATURE_NAMES
